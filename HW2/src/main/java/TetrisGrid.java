0601adcd0d827c028c3cb85667ce606d9fdc8d36
dunghkt213 <dung1990hk@gmail.com>
2024-09-26 13:34:20 +0700

    TetrisGrid

--- HW2/src/main/java/TetrisGrid.java
+++ HW2/src/main/java/TetrisGrid.java
@@ -1,29 +1,49 @@
 //
-// TetrisGrid encapsulates a tetris board and has
-// a clearRows() capability.
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
 public class TetrisGrid {
-	
-	/**
-	 * Constructs a new instance with the given grid.
-	 * Does not make a copy.
-	 * @param grid
-	 */
-	public TetrisGrid(boolean[][] grid) {
-	}
-	
-	
-	/**
-	 * Does row-clearing on the grid (see handout).
-	 */
-	public void clearRows() {
-	}
-	
-	/**
-	 * Returns the internal 2d grid array.
-	 * @return 2d grid array
-	 */
-	boolean[][] getGrid() {
-		return null; // YOUR CODE HERE
-	}
+    private boolean[][] grid;
+
+    public TetrisGrid(boolean[][] grid) {
+        this.grid = new boolean[grid.length][];
+
+        for(int i = 0; i < grid.length; ++i) {
+            this.grid[i] = (boolean[])grid[i].clone();
+        }
+
+    }
+
+    public void clearRows() {
+        for(int i = this.grid.length - 2; i >= 0; --i) {
+            for(int j = 0; j < this.grid[i].length; ++j) {
+                if (this.grid[i][j] && !this.grid[i + 1][j]) {
+                    this.grid[i][j] = false;
+                    this.grid[i + 1][j] = true;
+                }
+            }
+        }
+
+    }
+
+    boolean[][] getGrid() {
+        return this.grid;
+    }
+
+    public static void main(String[] args) {
+        boolean[][] grid = new boolean[][]{{true, false, true}, {false, true, false}, {true, true, false}};
+        TetrisGrid grid1 = new TetrisGrid(grid);
+        grid1.clearRows();
+        boolean[][] grid2 = grid1.getGrid();
+
+        for(int i = 0; i < grid2.length; ++i) {
+            for(int j = 0; j < grid2[i].length; ++j) {
+                System.out.print(grid2[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
 }
